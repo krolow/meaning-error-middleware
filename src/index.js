6020b17c2261c5b37a2273eb338472ea7bacab60
Vinícius Krolow <vinicius.krolow@bravi.com.br>
2015-10-21 09:05:23 -0200

    Add basic middleware to handle meaning-errors

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import { MeaningError } from 'meaning-error';
+
+export default function (err, req, res, next) {
+  if (!(err instanceof MeaningError)) {
+    return next();
+  }
+
+  const data = {
+    error_name: err.name,
+    error_message: err.message,
+    status_code: err.getCode(),
+    error: err.getError ? err.getError() : undefined,
+  };
+
+  return res
+    .status(err.getCode())
+    .send(data);
+}
